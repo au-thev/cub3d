ab480c43f701a2b83d94bee539ecaa7cc9cd75e9
Pascal <pnguyen-@student.42.fr>
2024-06-18 21:13:21 +0200

    add error message for bad args, check filename ext

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: autheven <autheven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 07:01:37 by autheven          #+#    #+#             */
-/*   Updated: 2024/06/18 21:04:54 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/06/18 21:12:58 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,13 +72,25 @@ int	mlx_start(t_cub3d *cub3d)
 	return (0);
 }
 
+int	is_valid_filename(char const path[])
+{
+	char const	*extension = ft_strrchr(path, '.');
+
+	if (extension == NULL)
+		return (0);
+	return (ft_strncmp(extension, ".cub", 5) == 0)
+}
+
 int	main(int argc, char **argv)
 {
 	t_cub3d	cub3d;
 	int		i;
 
 	if (argc != 2)
+	{
+		ft_putstr_fd("Usage : ./cub3d <filename>\n", STDERR_FILENO);
 		return (1);
+	}
 	if (mlx_start(&cub3d))
 	{
 		ft_putstr_fd("Failed to initialize mlx!\n", STDERR_FILENO);
@@ -88,7 +100,7 @@ int	main(int argc, char **argv)
 	while (i-- > 0)
 		cub3d.level.texture[i].loaded = 0;
 	cub3d.level.map = 0;
-	if (!load_level(&cub3d, argv[1]))
+	if (is_valid_filename(argv[1]) && !load_level(&cub3d, argv[1]))
 		if (!check_params(&cub3d))
 			mlx_loop(cub3d.mlx.ptr);
 	free_game(&cub3d);
